8c045d2fd72b886f83e56f2683ed8e5a739d87b2
Graham MacDonald <grahamamacdonald@gmail.com>
2016-01-17 09:38:51 +0000

    Abstract GL code

--- rt.go
+++ rt.go
@@ -2,14 +2,8 @@ package main
 
 import (
 	"fmt"
-	_ "image/png"
-	"math/rand"
-	"runtime"
-	"strings"
 
-	"github.com/go-gl/gl/all-core/gl"
-	"github.com/go-gl/glfw/v3.1/glfw"
-	"github.com/go-gl/mathgl/mgl32"
+	"github.com/gmacd/rt/support"
 )
 
 const windowWidth = 600
@@ -19,224 +13,15 @@ const renderbufferHeight = 600
 
 func init() {
 	// This is needed to arrange that main() runs on main thread.
-	runtime.LockOSThread()
+	//runtime.LockOSThread()
 }
 
 func main() {
-	err := glfw.Init()
-	if err != nil {
-		panic(err)
-	}
-	defer glfw.Terminate()
+	fmt.Println("=== rt ===")
 
-	glfw.WindowHint(glfw.Resizable, glfw.False)
-	glfw.WindowHint(glfw.ContextVersionMajor, 4)
-	glfw.WindowHint(glfw.ContextVersionMinor, 1)
-	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
-	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
-	window, err := glfw.CreateWindow(windowWidth, windowHeight, "rt", nil, nil)
-	if err != nil {
-		panic(err)
-	}
-	window.MakeContextCurrent()
+	renderer := support.NewGlRenderer(renderbufferWidth, renderbufferHeight)
+	defer renderer.Shutdown()
 
-	if err := gl.Init(); err != nil {
-		panic(err)
-	}
-
-	version := gl.GoStr(gl.GetString(gl.VERSION))
-	fmt.Println("OpenGL version", version)
-
-	program, err := createScreenShader()
-	if err != nil {
-		panic(err)
-	}
-
-	gl.UseProgram(program)
-
-	projection := mgl32.Ortho2D(-1, 1, 1, -1)
-	projectionUniform := gl.GetUniformLocation(program, gl.Str("projection\x00"))
-	gl.UniformMatrix4fv(projectionUniform, 1, false, &projection[0])
-
-	textureUniform := gl.GetUniformLocation(program, gl.Str("tex\x00"))
-	gl.Uniform1i(textureUniform, 0)
-
-	texture := createScreenTexture()
-
-	var vao uint32
-	gl.GenVertexArrays(1, &vao)
-	gl.BindVertexArray(vao)
-
-	var quadVertices = []float32{
-		//  X, Y, Z, U, V
-		1.0, -1.0, 0.0, 1.0, 0.0,
-		-1.0, -1.0, 0.0, 0.0, 0.0,
-		1.0, 1.0, 0.0, 1.0, 1.0,
-		-1.0, -1.0, 0.0, 0.0, 0.0,
-		-1.0, 1.0, 0.0, 0.0, 1.0,
-		1.0, 1.0, 0.0, 1.0, 1.0,
-	}
-
-	var vbo uint32
-	gl.GenBuffers(1, &vbo)
-	gl.BindBuffer(gl.ARRAY_BUFFER, vbo)
-	gl.BufferData(gl.ARRAY_BUFFER, len(quadVertices)*4, gl.Ptr(quadVertices), gl.STATIC_DRAW)
-
-	vertAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vert\x00")))
-	gl.EnableVertexAttribArray(vertAttrib)
-	gl.VertexAttribPointer(vertAttrib, 3, gl.FLOAT, false, 5*4, gl.PtrOffset(0))
-
-	texCoordAttrib := uint32(gl.GetAttribLocation(program, gl.Str("vertTexCoord\x00")))
-	gl.EnableVertexAttribArray(texCoordAttrib)
-	gl.VertexAttribPointer(texCoordAttrib, 2, gl.FLOAT, false, 5*4, gl.PtrOffset(3*4))
-
-	gl.Enable(gl.DEPTH_TEST)
-	gl.ClearColor(1.0, 1.0, 1.0, 1.0)
-
-	for !window.ShouldClose() {
-		gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-
-		gl.UseProgram(program)
-		gl.BindVertexArray(vao)
-		updateScreenTexture(texture)
-		gl.DrawArrays(gl.TRIANGLES, 0, 6)
-
-		window.SwapBuffers()
-		glfw.PollEvents()
-	}
-}
-
-func createScreenShader() (uint32, error) {
-	var vertexShader = `
-#version 330
-
-uniform mat4 projection;
-in vec3 vert;
-in vec2 vertTexCoord;
-out vec2 fragTexCoord;
-
-void main() {
-    fragTexCoord = vertTexCoord;
-    gl_Position = projection * vec4(vert, 1);
-}
-` + "\x00"
-
-	var fragmentShader = `
-#version 330
-
-uniform sampler2D tex;
-in vec2 fragTexCoord;
-out vec4 outputColor;
-
-void main() {
-    outputColor = texture(tex, fragTexCoord);
-}
-` + "\x00"
-
-	return newProgram(vertexShader, fragmentShader)
-}
-
-func newProgram(vertexShaderSource, fragmentShaderSource string) (uint32, error) {
-	vertexShader, err := compileShader(vertexShaderSource, gl.VERTEX_SHADER)
-	if err != nil {
-		return 0, err
-	}
-
-	fragmentShader, err := compileShader(fragmentShaderSource, gl.FRAGMENT_SHADER)
-	if err != nil {
-		return 0, err
-	}
-
-	program := gl.CreateProgram()
-
-	gl.AttachShader(program, vertexShader)
-	gl.AttachShader(program, fragmentShader)
-	gl.LinkProgram(program)
-
-	var status int32
-	gl.GetProgramiv(program, gl.LINK_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetProgramiv(program, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetProgramInfoLog(program, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to link program: %v", log)
-	}
-
-	gl.DeleteShader(vertexShader)
-	gl.DeleteShader(fragmentShader)
-
-	return program, nil
-}
-
-func compileShader(source string, shaderType uint32) (uint32, error) {
-	shader := gl.CreateShader(shaderType)
-
-	csource := gl.Str(source)
-	gl.ShaderSource(shader, 1, &csource, nil)
-	gl.CompileShader(shader)
-
-	var status int32
-	gl.GetShaderiv(shader, gl.COMPILE_STATUS, &status)
-	if status == gl.FALSE {
-		var logLength int32
-		gl.GetShaderiv(shader, gl.INFO_LOG_LENGTH, &logLength)
-
-		log := strings.Repeat("\x00", int(logLength+1))
-		gl.GetShaderInfoLog(shader, logLength, nil, gl.Str(log))
-
-		return 0, fmt.Errorf("failed to compile %v: %v", source, log)
-	}
-
-	return shader, nil
-}
-
-func createScreenTexture() uint32 {
-	var texture uint32
-	gl.GenTextures(1, &texture)
-	gl.ActiveTexture(gl.TEXTURE0)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
-	gl.TexParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE)
-	gl.TexImage2D(
-		gl.TEXTURE_2D,
-		0,
-		gl.RGBA32F,
-		renderbufferWidth,
-		renderbufferHeight,
-		0,
-		gl.RGBA,
-		gl.FLOAT,
-		nil)
-
-	return texture
-}
-
-func updateScreenTexture(texture uint32) {
-	// Yellow noise
-	pixels := make([]float32, 4*renderbufferWidth*renderbufferHeight)
-	for i := 0; i < 4*renderbufferWidth*renderbufferHeight; i += 4 {
-		yellow := rand.Float32()/2.0 + 0.5
-		pixels[i] = yellow
-		pixels[i+1] = yellow
-		pixels[i+2] = 0
-		pixels[i+3] = 1.0
-	}
-
-	gl.ActiveTexture(gl.TEXTURE0)
-	gl.BindTexture(gl.TEXTURE_2D, texture)
-	gl.TexSubImage2D(
-		gl.TEXTURE_2D,
-		0,
-		0,
-		0,
-		renderbufferWidth,
-		renderbufferHeight,
-		gl.RGBA,
-		gl.FLOAT,
-		gl.Ptr(pixels))
+	renderer.Init()
+	renderer.Loop()
 }
