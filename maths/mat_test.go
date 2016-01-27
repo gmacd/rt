176ca1fc4e18fa52e7e9edfbf33aeed7fb0fe4d0
Graham MacDonald <grahamamacdonald@gmail.com>
2016-01-27 22:16:50 +0000

    MatMul benchmark BenchmarkMatMul-8	10000000	       170 ns/op

--- maths/mat_test.go
+++ maths/mat_test.go
@@ -39,3 +39,16 @@ func TestMulIdent(t *testing.T) {
 	test.Equal(t, m, m1, "m is not identity")		
 }
 
+func BenchmarkMatMul(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		idx := float32(i)
+		m1 := NewMat(
+			idx+1, idx+2, idx+3, idx+4, idx+5, idx+6, idx+7, idx+8,
+			idx+9, idx+10, idx+11, idx+12, idx+13, idx+14, idx+15, idx+16)
+		m2 := NewMat(
+			idx+1000, idx+2000, idx+3000, idx+4000, idx+5000, idx+6000, idx+7000, idx+8000,
+			idx+9000, idx+10000, idx+11000, idx+12000, idx+13000, idx+14000, idx+15000, idx+16000)
+		
+		m1.Mul(&m2)
+	}
+}
