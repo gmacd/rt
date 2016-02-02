372a759cbd4c6fff223a5240be7ac603b63dd8b1
Graham MacDonald <grahamamacdonald@gmail.com>
2016-02-02 21:56:50 +0000

    Temporarily remove asm. BenchmarkMatMul-8 	50000000	        34.7 ns/op BenchmarkMatMuls-8	100000000	        18.4 ns/op

--- maths/mat_new.go
+++ maths/mat_new.go
@@ -4,28 +4,24 @@ import (
 	"math"
 )
 
-func NewMat(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p float32) *Mat {
-	return &Mat{
+func NewMat(a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p float32) Mat {
+	return Mat{
 		{a, b, c, d},
 		{e, f, g, h},
 		{i, j, k, l},
 		{m, n, o, p}}
 }
 
-func NewMatZero() *Mat {
-	return &Mat{}
-}
-
-func NewMatIdent() *Mat {
-	return &Mat{
+func NewMatIdent() Mat {
+	return Mat{
 		{1.0, 0.0, 0.0, 0.0},
 		{0.0, 1.0, 0.0, 0.0},
 		{0.0, 0.0, 1.0, 0.0},
 		{0.0, 0.0, 0.0, 1.0}}
 }
 
-func NewMatScale(x, y, z float32) *Mat {
-	return &Mat{
+func NewMatScale(x, y, z float32) Mat {
+	return Mat{
 		{x, 0.0, 0.0, 0.0},
 		{0.0, y, 0.0, 0.0},
 		{0.0, 0.0, z, 0.0},
@@ -33,11 +29,11 @@ func NewMatScale(x, y, z float32) *Mat {
 }
 
 // Left-handed (z into screen is +ve, y up is +ve, x right is +ve
-func NewMatPerspective(fov, near, far float32) *Mat {
+func NewMatPerspective(fov, near, far float32) Mat {
 	// TODO Optimize to build mat directly
 	i33 := far / (far - near)
 	i34 := i33 * -near
-	perspMat := &Mat{
+	perspMat := Mat{
 		{1.0, 0.0, 0.0, 0.0},
 		{0.0, 1.0, 0.0, 0.0},
 		{0.0, 0.0, i33, i34},
@@ -45,6 +41,6 @@ func NewMatPerspective(fov, near, far float32) *Mat {
 
 	invTanAngle := float32(1.0 / math.Tan(float64(DegToRad(fov)/2.0)))
 	scaleMat := NewMatScale(invTanAngle, invTanAngle, 1)
-	result := scaleMat.Mul(perspMat)
+	result := scaleMat.Mul(&perspMat)
 	return result
 }
--- maths/mat_test.go
+++ maths/mat_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestMatSizeCorrect(t *testing.T) {
 	m := NewMatIdent()
-	test.Equal(t, 64, int(unsafe.Sizeof(*m)), "Mat wrong size")
+	test.Equal(t, 64, int(unsafe.Sizeof(m)), "Mat wrong size")
 }
 
 func TestNewMatIdent(t *testing.T) {
@@ -19,8 +19,8 @@ func TestNewMatIdent(t *testing.T) {
 		0, 1, 0, 0,
 		0, 0, 1, 0,
 		0, 0, 0, 1)
-	if *m1 != *m2 {
-		t.Errorf("Expected identity, but got: %v", *m2)
+	if m1 != m2 {
+		t.Errorf("Expected identity, but got: %v", m2)
 	}
 }
 
@@ -31,37 +31,36 @@ func TestNewMatScale(t *testing.T) {
 		0, 3, 0, 0,
 		0, 0, -4, 0,
 		0, 0, 0, 1)
-	if *m1 != *expected {
-		t.Errorf("Expected %v, but got: %v", expected, *m1)
+	if m1 != expected {
+		t.Errorf("Expected %v, but got: %v", expected, m1)
 	}
 }
 
 func TestMulIdent(t *testing.T) {
 	i := NewMatIdent()
-	m := i.Mul(i)
-	if *m != *i {
-		t.Errorf("Expected %v, but got: %v", *i, *m)
+	m := i.Mul(&i)
+	if m != i {
+		t.Errorf("Expected %v, but got: %v", i, m)
 	}
 }
 
 func TestMuls(t *testing.T) {
 	i := NewMatIdent()
-	m := &Mat{}
-	Muls(5, i, m)
+	m := i.Muls(5)
 	expected := NewMat(
 		5, 0, 0, 0,
 		0, 5, 0, 0,
 		0, 0, 5, 0,
 		0, 0, 0, 5)
 	
-	if *m != *expected {
-		t.Errorf("Expected %v, but found %v.", *expected, *m)
+	if m != expected {
+		t.Errorf("Expected %v, but found %v.", expected, m)
 	}
 }
 
 // Benchmarks
 
-var matResult *Mat
+var matResult Mat
 
 func BenchmarkMatMul(b *testing.B) {
 	m1 := NewMat(
@@ -73,9 +72,9 @@ func BenchmarkMatMul(b *testing.B) {
 		
 	b.ResetTimer()
 	
-	var mr *Mat
+	var mr Mat
 	for i := 0; i < b.N; i++ {
-		mr = m1.Mul(m2)
+		mr = m1.Mul(&m2)
 	}
 	matResult = mr
 }
@@ -87,9 +86,9 @@ func BenchmarkMatMuls(b *testing.B) {
 		
 	b.ResetTimer()
 	
-	mr := &Mat{}
+	var mr Mat
 	for i := 0; i < b.N; i++ {
-		Muls(float32(i), m1, mr)
+		mr = m1.Muls(float32(i))
 	}
 	matResult = mr
 }
